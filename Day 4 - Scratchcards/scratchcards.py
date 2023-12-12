705bd7a6f2a59247a4ebc591074d1d77f68f017e
cineg <cineg7@gmail.com>
2023-12-12 22:47:53 +0100

    day 4 - part 2

--- Day 4 - Scratchcards/scratchcards.py
+++ Day 4 - Scratchcards/scratchcards.py
@@ -1,10 +1,18 @@
 from input import INPUT
+from dataclasses import dataclass
+
+
+@dataclass
+class Card:
+    copies: int = 1
+    winning_points: int = 0
 
 
 def main():
     data: list[str] = INPUT.splitlines()
-
     total_points: int = 0
+    data_cards: list[Card] = []
+    total_cards: int = 0
 
     for card in data:
         winning_numbers: dict = get_winning_numers(card)
@@ -17,6 +25,24 @@ def main():
         if card_matches != -1:
             total_points += pow(2, card_matches)
 
+        item: Card = Card(winning_points=card_matches + 1)
+        data_cards.append(item)
+
+    index: int = 0
+    while index < len(data_cards):
+        i: int = 1
+        copies_to_add: int = data_cards[index].winning_points
+        if copies_to_add != 0:
+            while index + i <= len(data_cards):
+                data_cards[index + i].copies += 1 * data_cards[index].copies
+                if i != copies_to_add:
+                    i += 1
+                else:
+                    break
+        total_cards += data_cards[index].copies
+        index += 1
+
+    print(total_cards)
     return total_points
 
 
