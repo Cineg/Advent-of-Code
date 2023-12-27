844ff3f931be9a770e4f5b5921c49d4748a9d8c6
cineg <cineg7@gmail.com>
2023-12-27 14:24:25 +0100

    day 13 - part 2

--- Day 13 - Point of Incidence/point.py
+++ Day 13 - Point of Incidence/point.py
@@ -25,10 +25,32 @@ def find_reflection(pattern: list[str]) -> int:
         top_part = top_part[: len(bottom_part)]
         bottom_part = bottom_part[: len(top_part)]
 
-        if top_part == bottom_part:
+        if is_smudged(top_part, bottom_part):
             reflection_point = i
 
+        # if top_part == bottom_part:
+        #     reflection_point = i
+
     return reflection_point
+
+
+def is_smudged(top_part: list[str], bottom_part: list[str]) -> bool:
+    differences: int = 0
+    line_length: int = len(top_part[0])
+    for i in range(0, len(top_part)):
+        for char in range(0, line_length):
+            if top_part[i][char] != bottom_part[i][char]:
+                differences += 1
+
+            if differences > 1:
+                break
+        if differences > 1:
+            break
+
+    if differences == 1:
+        return True
+
+    return False
 
 
 if __name__ == "__main__":
