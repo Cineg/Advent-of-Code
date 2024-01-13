37f0d9cd7beac3a09248c46242df2419341b445d
cineg <cineg7@gmail.com>
2024-01-13 16:29:45 +0100

    day 17 - part 1 (kind of)

--- Day 17 - Clumsy Crucible/crucible.py
+++ Day 17 - Clumsy Crucible/crucible.py
@@ -1,68 +1,99 @@
 from math import inf
+from heapq import heappop, heappush
+
+
+class Path:
+    def __init__(self, row: int, col: int, value: int, path: list, dir: list) -> None:
+        self.row: int = row
+        self.col: int = col
+        self.value: int = value
+        self.path: list[tuple] = path
+        self.directions: list[tuple] = dir
+
+    def __lt__(self, _):
+        return (self.value, self.row, self.col)
+
+    def is_direction_change_required(self) -> tuple:
+        ### returns (0,0) if path is valid
+        ### otherwise returns invalid path direction
+        last_steps: list[tuple] = self.directions[-3:]
+        if len(last_steps) != 3:
+            return (0, 0)
+
+        if last_steps[0] == last_steps[1] == last_steps[2]:
+            return last_steps[0]
+
+        return (0, 0)
+
+    def no_backward(self) -> tuple:
+        row, col = self.directions[-1]
+        row = -row
+        col = -col
+
+        return (row, col)
 
 
 def main() -> None:
     grid: list[list[int]] = get_grid()
-    grid_values: list[list[float]] = get_inf_grid(grid)
-    grid_values[0][0] = 0
 
-    # value, (row, column), (row direction, column direction), number of moves in direction
-    queue: list = [(0, (0, 0), (0, 0), 0)]
-    seen_nodes = set()
+    queue: list[Path] = [Path(0, 0, 0, [(0, 0)], [(0, 0)])]
 
+    end_path_items: list[Path] = []
     min_value: float = inf
 
+    values: dict = {}
+
     while queue:
-        for node in queue:
-            node_to_be_seen = (node[1], node[2], node[3])
-            queue.pop(queue.index(node))
+        current_path: Path = heappop(queue)
 
-            # skip if seen
-            if node_to_be_seen in seen_nodes:
+        if current_path.value > min_value:
+            continue
+
+        path_check = tuple(current_path.path[-3:])
+        if path_check in values:
+            if values[path_check] < current_path.value:
+                continue
+            else:
+                values[path_check] = current_path.value
+        else:
+            values[path_check] = current_path.value
+
+        forbidden_direction: tuple = current_path.is_direction_change_required()
+        for direction in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+            if (
+                forbidden_direction == direction
+                and direction != current_path.no_backward()
+            ):
                 continue
 
-            seen_nodes.add(node_to_be_seen)
+            row: int = current_path.row + direction[0]
+            col: int = current_path.col + direction[1]
 
-            value, cell, direction, moves = node
-
-            if value > min_value:
+            if (
+                not check_for_bounds((row, col), grid)
+                or (row, col) in current_path.path
+            ):
                 continue
 
-            if len(grid) - 1 == cell[0] and len(grid[0]) - 1 == cell[1]:
-                min_value = min(min_value, value)
+            path_so_far: list[tuple] = current_path.path.copy()
+            direction_so_far: list[tuple] = current_path.directions.copy()
+            path_so_far.append((row, col))
+            direction_so_far.append(direction)
 
-            if moves < 3 and direction != (0, 0):
-                new_cell = cell[0] + direction[0], cell[1] + direction[1]
-                if check_for_bounds(new_cell, grid):
-                    new_value = value + grid[new_cell[0]][new_cell[1]]
-                    queue.append((new_value, new_cell, direction, moves + 1))
+            new_path: Path = Path(
+                row,
+                col,
+                current_path.value + grid[row][col],
+                path_so_far,
+                direction_so_far,
+            )
 
-                    grid_values[new_cell[0]][new_cell[1]] = min(
-                        new_value, grid_values[new_cell[0]][new_cell[1]]
-                    )
+            if row == len(grid) - 1 and col == len(grid[0]) - 1:
+                end_path_items.append(new_path)
+                min_value = min(min_value, new_path.value)
 
-            for new_direction in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                # cant be the same direction
-                if new_direction == direction:
-                    continue
-
-                # cant move backwards
-                if (
-                    new_direction[0] == -direction[0]
-                    and new_direction[1] == -direction[1]
-                ):
-                    continue
-
-                new_cell = cell[0] + new_direction[0], cell[1] + new_direction[1]
-
-                # check for out of bonds
-                if check_for_bounds(new_cell, grid):
-                    new_value = value + grid[new_cell[0]][new_cell[1]]
-                    queue.append((new_value, new_cell, new_direction, 1))
-
-                    grid_values[new_cell[0]][new_cell[1]] = min(
-                        new_value, grid_values[new_cell[0]][new_cell[1]]
-                    )
+            else:
+                heappush(queue, new_path)
 
     print(min_value)
 
@@ -72,9 +103,7 @@ def check_for_bounds(new_cell, grid) -> bool:
 
 
 def get_grid() -> list[list[int]]:
-    input: list[str] = (
-        open("Day 17 - Clumsy Crucible\input_test.txt").read().splitlines()
-    )
+    input: list[str] = open("Day 17 - Clumsy Crucible\input.txt").read().splitlines()
 
     grid: list[list[int]] = []
     for line in input:
@@ -85,19 +114,6 @@ def get_grid() -> list[list[int]]:
         grid.append(grid_line)
 
     return grid
-
-
-def get_inf_grid(grid: list[list[int]]) -> list[list[float]]:
-    new_grid: list[list[float]] = []
-
-    for _ in grid:
-        line: list[float] = []
-        for __ in _:
-            line.append(inf)
-
-        new_grid.append(line)
-
-    return new_grid
 
 
 if __name__ == "__main__":
