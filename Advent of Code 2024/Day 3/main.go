a91ec66ba4addca333a7d42fd5b68abc2a87d937
Cineg <cineg7@gmail.com>
2024-12-26 13:35:35 +0100

    day 3 part 2

--- Advent of Code 2024/Day 3/main.go
+++ Advent of Code 2024/Day 3/main.go
@@ -11,10 +11,12 @@ import (
 )
 
 var regex_for_part_1 *regexp.Regexp = regexp.MustCompile(`(?m)mul\(\d{1,3},\d{1,3}\)`)
+var regex_for_part_2 *regexp.Regexp = regexp.MustCompile(`(?m)do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\)`)
 
 func main() {
 	input_string := readFile()
 	fmt.Println(part1(input_string))
+	fmt.Println(part2(input_string))
 }
 
 func part1(input_string string) int {
@@ -27,6 +29,28 @@ func part1(input_string string) int {
 		result += (castToInt(split[0]) * castToInt(split[1]))
 	}
 
+	return result
+}
+
+func part2(input_string string) int {
+	var result int = 0
+	data := regex_for_part_2.FindAllString(input_string, -1)
+	var flag bool = true
+	for _, v := range data {
+		if v == "do()" {
+			flag = true
+			continue
+		} else if v == "don't()" {
+			flag = false
+			continue
+		}
+		if flag {
+			split := strings.Split(v, "(")
+			split = strings.Split(split[1], ")")
+			split = strings.Split(split[0], ",")
+			result += (castToInt(split[0]) * castToInt(split[1]))
+		}
+	}
 	return result
 }
 
