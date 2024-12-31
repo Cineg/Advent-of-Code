53d94e291a601ba5dbc5a23c4bad2251f8f10f0f
Cineg <cineg7@gmail.com>
2024-12-31 13:45:48 +0100

    Day 4 part 2

--- Advent of Code 2024/Day 4/main.go
+++ Advent of Code 2024/Day 4/main.go
@@ -38,6 +38,7 @@ var possibilities = [8]Possibilities{
 func main() {
 	input_data := readFile()
 	fmt.Println(part1(input_data))
+	fmt.Println(part2(input_data))
 }
 
 func part1(input_data []string) int {
@@ -47,13 +48,40 @@ func part1(input_data []string) int {
 	result := 0
 	for i := 0; i < input_data_x; i++ {
 		for j := 0; j < input_data_y; j++ {
-			result += checkMatch(input_data, Coordinate{i, j})
+			result += checkMatch(input_data, Coordinate{i, j}, possibilities[:])
 		}
 	}
 	return result
 }
 
-func checkMatch(input_data []string, coord Coordinate) int {
+func part2(input_data []string) int {
+	input_data_x := len(input_data[0])
+	input_data_y := len(input_data)
+
+	result := 0
+
+	possibilities := [4]string{"MMSS", "SSMM", "MSMS", "SMSM"}
+	//	M	M		S	S		M	S		S	M
+	//    A			  A			  A		  	  A
+	//	S	S		M	M		M	S		S	M
+
+	for i := 1; i < input_data_x-1; i++ {
+		for j := 1; j < input_data_y-1; j++ {
+			if string(input_data[i][j]) != "A" {
+				continue
+			}
+			str := string(input_data[i-1][j-1]) + string(input_data[i-1][j+1]) + string(input_data[i+1][j-1]) + string(input_data[i+1][j+1])
+			for _, v := range possibilities {
+				if v == str {
+					result += 1
+				}
+			}
+		}
+	}
+	return result
+}
+
+func checkMatch(input_data []string, coord Coordinate, possibilities []Possibilities) int {
 	result := 0
 	for _, possibility := range possibilities {
 		word := ""
