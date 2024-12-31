b177678834e2624fedd6a02adf2c18b2c53f38de
Cineg <cineg7@gmail.com>
2024-12-31 13:22:52 +0100

    Day 4 part 1

---
+++ Advent of Code 2024/Day 4/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Coordinate struct {
+	x int
+	y int
+}
+
+type Position struct {
+	start Coordinate
+	end   Coordinate
+}
+
+type Possibilities struct {
+	offset [4]Coordinate
+}
+
+var possibilities = [8]Possibilities{
+	{offset: [4]Coordinate{{0, 0}, {0, 1}, {0, 2}, {0, 3}}},
+	{offset: [4]Coordinate{{0, 0}, {0, -1}, {0, -2}, {0, -3}}},
+
+	{offset: [4]Coordinate{{0, 0}, {1, 0}, {2, 0}, {3, 0}}},
+	{offset: [4]Coordinate{{0, 0}, {-1, 0}, {-2, 0}, {-3, 0}}},
+
+	{offset: [4]Coordinate{{0, 0}, {1, 1}, {2, 2}, {3, 3}}},       // left right diagonal down
+	{offset: [4]Coordinate{{0, 0}, {-1, -1}, {-2, -2}, {-3, -3}}}, // right to left diagonal up
+
+	{offset: [4]Coordinate{{0, 0}, {1, -1}, {2, -2}, {3, -3}}}, // right to left diagonal down
+	{offset: [4]Coordinate{{0, 0}, {-1, 1}, {-2, 2}, {-3, 3}}}, // left to right diagonal up
+}
+
+func main() {
+	input_data := readFile()
+	fmt.Println(part1(input_data))
+}
+
+func part1(input_data []string) int {
+	input_data_x := len(input_data[0])
+	input_data_y := len(input_data)
+
+	result := 0
+	for i := 0; i < input_data_x; i++ {
+		for j := 0; j < input_data_y; j++ {
+			result += checkMatch(input_data, Coordinate{i, j})
+		}
+	}
+	return result
+}
+
+func checkMatch(input_data []string, coord Coordinate) int {
+	result := 0
+	for _, possibility := range possibilities {
+		word := ""
+		for _, offset := range possibility.offset {
+			x := coord.x + offset.x
+			y := coord.y + offset.y
+			if x < 0 || x >= len(input_data) {
+				break
+			}
+			if y < 0 || y >= len(input_data) {
+				break
+			}
+
+			word += string(input_data[y][x])
+		}
+
+		if word == "XMAS" {
+			result += 1
+		}
+	}
+	return result
+}
+
+func readFile() []string {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows := make([]string, 0)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		rows = append(rows, scanner.Text())
+	}
+
+	return rows
+}
