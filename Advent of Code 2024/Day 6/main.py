0d7af96090f57d204768d105053b9c37f6ab273a
Cineg <cineg7@gmail.com>
2025-01-20 22:11:15 +0100

    day 6 part 2

--- Advent of Code 2024/Day 6/main.py
+++ Advent of Code 2024/Day 6/main.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from pathlib import Path
 
 
@@ -38,6 +37,25 @@ def day1() -> int:
     return len(seen)
 
 
+def day2() -> int:
+    tiles: list[list[str]] = read_map()
+    start_pos: tuple[int, int] = get_start_position(tiles)
+
+    obstacle: int = 0
+    for r in range(len(tiles)):
+        for c in range(len(tiles[0])):
+            if tiles[r][c] != ".":
+                continue
+
+            tiles[r][c] = "#"
+            if loops(tiles, start_pos):
+                obstacle += 1
+
+            tiles[r][c] = "."
+
+    return obstacle
+
+
 def read_map() -> list[list[str]]:
     input: str = open(Path(Path(__file__).parent, "input.txt"), "r").read()
     path: list[str] = input.split("\n")
@@ -57,5 +75,38 @@ def get_start_position(tiles: list[list[str]]) -> tuple[int, int]:
     return (-1, -1)
 
 
+def loops(tiles: list[list[str]], position: tuple[int, int]) -> bool:
+    seen = set()
+
+    directions: list[tuple[int, int]] = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    direction: int = 0
+
+    while True:
+        seen.add((position, direction))
+        next: tuple[int, int] = (
+            position[0] + directions[direction][0],
+            position[1] + directions[direction][1],
+        )
+
+        if not (
+            next[0] >= 0
+            and next[0] < len(tiles)
+            and next[1] >= 0
+            and next[1] < len(tiles[0])
+        ):
+            return False
+
+        if tiles[next[0]][next[1]] == "#":
+            direction += 1
+            direction %= 4
+            continue
+
+        if (next, direction) in seen:
+            return True
+
+        position = next
+
+
 if __name__ == "__main__":
     print(day1())
+    print(day2())
