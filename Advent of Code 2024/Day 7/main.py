8aee0cc0a2cc367750125cd6977d4d8d363d2542
Cineg <cineg7@gmail.com>
2025-01-21 17:41:31 +0100

    day 7 part 1

---
+++ Advent of Code 2024/Day 7/main.py
@@ -0,0 +1,35 @@
+from collections import deque
+from pathlib import Path
+
+data: list[str] = (
+    open(Path(Path(__file__).parent, "input.txt"), "r").read().splitlines()
+)
+
+
+def day1() -> int:
+    total: int = 0
+    for row in data:
+        result, items = row.split(": ")
+        result = int(result)
+
+        numbers = deque([int(i) for i in items.split(" ")])
+
+        results: list[int] = [numbers.popleft()]
+        while numbers:
+            num = numbers.popleft()
+            temp: list[int] = []
+            while results:
+                x: int = results.pop()
+                temp.append(num * x)
+                temp.append(num + x)
+
+            results = temp
+
+        if result in results:
+            total += result
+
+    return total
+
+
+if __name__ == "__main__":
+    print(day1())
